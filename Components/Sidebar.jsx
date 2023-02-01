e3f471df3cea20aa3826e78552cef77f77dd0875
Faisal <biddafaisal@gmail.com>
2023-02-01 23:30:44 +0530

    first commit

---
+++ Components/Sidebar.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { HomeIcon,MagnifyingGlassIcon,BuildingLibraryIcon,PlayCircleIcon,HeartIcon,RssIcon} from '@heroicons/react/24/outline'
+
+function Sidebar() {
+    return (
+        <div className='text-gray-500 p-5 text-sm border-r border-gray-900'>
+            <div className="space-y-4">
+                <button className='flex items-center space-x-2 hover:text-white'>
+                    <HomeIcon className='w-5 h-5'/>
+                    <p>Home</p>
+                </button>
+                <button className='flex items-center space-x-2 hover:text-white'>
+                    <MagnifyingGlassIcon className='w-5 h-5'/>
+                    <p>Search</p>
+                </button>
+                <button className='flex items-center space-x-2 hover:text-white'>
+                    <BuildingLibraryIcon className='w-5 h-5'/>
+                    <p>Your Library</p>
+                </button>
+                <hr className='border-gray-900'/>
+                <button className='flex items-center space-x-2 hover:text-white'>
+                    <PlayCircleIcon className='w-5 h-5'/>
+                    <p>Create Playlist</p>
+                </button>
+                <button className='flex items-center space-x-2 hover:text-white'>
+                    <HeartIcon className='w-5 h-5'/>
+                    <p>Liked Songs</p>
+                </button>
+                <button className='flex items-center space-x-2 hover:text-white'>
+                    <RssIcon className='w-5 h-5'/>
+                    <p>Your episodes</p>
+                </button>
+                <hr className='border-gray-900'/>
+
+                {/* {playlists} */}
+                <p className='flex items-center space-x-2 hover:text-white'>playlists</p>
+                <p className='flex items-center space-x-2 hover:text-white'>playlists</p>
+                <p className='flex items-center space-x-2 hover:text-white'>playlists</p>
+                <p className='flex items-center space-x-2 hover:text-white'>playlists</p>
+                <p className='flex items-center space-x-2 hover:text-white'>playlists</p>
+                <p className='flex items-center space-x-2 hover:text-white'>playlists</p>
+                <p className='flex items-center space-x-2 hover:text-white'>playlists</p>
+                <p className='flex items-center space-x-2 hover:text-white'>playlists</p>
+                <p className='flex items-center space-x-2 hover:text-white'>playlists</p>
+                <p className='flex items-center space-x-2 hover:text-white'>playlists</p>
+                <p className='flex items-center space-x-2 hover:text-white'>playlists</p>
+                <p className='flex items-center space-x-2 hover:text-white'>playlists</p>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
